3c984fc20446fd16664884fab0a30740954bda62
defan <defan@list.ru>
2012-08-10 14:03:18 +0300

    Feature #3 :: Admin - Devises 1. list format date 2. add twig filter datefmt_format

---
+++ src/Application/Twig/DateFmtFormatExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Twig;
+
+use Twig_Extension;
+use Twig_Filter_Method;
+
+
+class DateFmtFormatExtension extends Twig_Extension
+{
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            'datefmt_format' => new Twig_Filter_Method($this, 'datefmtFormatFilter'),
+        );
+    }
+
+    /**
+     * @param \DateTime $datetime
+     * @param null|string $format
+     * @return string
+     */
+    public function datefmtFormatFilter($datetime, $format = null)
+    {
+        $dateFormat = is_int($format) ? $format : \IntlDateFormatter::MEDIUM;
+        $timeFormat = \IntlDateFormatter::NONE;
+        $calendar = \IntlDateFormatter::GREGORIAN;
+        $pattern = is_string($format) ? $format : null;
+
+        $formatter = new \IntlDateFormatter(
+            \Locale::getDefault(),
+            $dateFormat,
+            $timeFormat,
+            null, //$datetime->getTimezone()->getName(),
+            $calendar,
+            $pattern
+        );
+        $formatter->setLenient(false);
+        $timestamp = $datetime->getTimestamp();
+
+        return $formatter->format($timestamp);
+    }
+
+    public function getName()
+    {
+        return 'datefmt_extension';
+    }
+}
