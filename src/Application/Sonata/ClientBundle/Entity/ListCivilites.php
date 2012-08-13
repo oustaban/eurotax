17b70f4ecfe90fe8ddee5861906db28dd3271b87
defan <defan@list.ru>
2012-08-13 20:45:11 +0300

    Feature #7 :: Admin Clients - Contacts - CRUD add List Civilite

---
+++ src/Application/Sonata/ClientBundle/Entity/ListCivilites.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Application\Sonata\ClientBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Application\Sonata\ClientBundle\Entity\ListCivilites
+ *
+ * @ORM\Table("et_list_civilite")
+ * @ORM\Entity
+ */
+class ListCivilites
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=200)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return ListCivilites
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(){
+        return $this->getName();
+    }
+}
