7146da5eeaccba1f19055857af3c49616b09fbd0
defan <defan@list.ru>
2012-08-21 20:01:48 +0300

    Feature #18 :: Client Operations - Data description A04-283-I Translate _toString

---
+++ src/Application/Sonata/ClientOperationsBundle/Controller/A04283IController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Application\Sonata\ClientOperationsBundle\Controller;
+
+use Application\Sonata\ClientOperationsBundle\Controller\AbstractTabsController as Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+
+class A04283IController extends Controller
+{
+    /**
+     * @var string
+     */
+    protected $_tabAlias = 'a04283i';
+    protected $_operationType = 'buy';
+
+
+}
