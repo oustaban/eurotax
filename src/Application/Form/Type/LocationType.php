da51dbb9bf68c37670027ca69aaaac464a0d7c45
Volodymyr <volodymyr@nakvasiuk.com>
2012-08-21 11:57:23 +0300

    Review #55 :: Admin Location fields - Last Layout modification Fixing fields width

--- src/Application/Form/Type/LocationType.php
+++ src/Application/Form/Type/LocationType.php
@@ -13,11 +13,11 @@ class LocationType extends AbstractType
     {
         $label = 'form.' . $this->getName() . '.';
         $builder
-            ->add('adresse_1' . $this->_extension, 'text', array('label' => $label . 'adresse' . $this->_extension . '_1', 'required' => false,))
-            ->add('adresse_2' . $this->_extension, 'text', array('label' => $label . 'adresse' . $this->_extension . '_2', 'required' => false,))
-            ->add('code_postal' . $this->_extension, 'text', array('label' => $label . 'CP' . $this->_extension, 'required' => false,))
-            ->add('ville' . $this->_extension, 'text', array('label' => $label . 'ville' . $this->_extension, 'required' => false,))
-            ->add('pays_id' . $this->_extension, 'country', array('label' => $label . 'pays' . $this->_extension, 'required' => true,));
+            ->add('adresse_1' . $this->_extension, 'text', array('attr'=>array('class'=>'span5'), 'label' => $label . 'adresse' . $this->_extension . '_1', 'required' => false,))
+            ->add('adresse_2' . $this->_extension, 'text', array('attr'=>array('class'=>'span5'), 'label' => $label . 'adresse' . $this->_extension . '_2', 'required' => false,))
+            ->add('code_postal' . $this->_extension, 'text', array('attr'=>array('class'=>'span5'), 'label' => $label . 'CP' . $this->_extension, 'required' => false,))
+            ->add('ville' . $this->_extension, 'text', array('attr'=>array('class'=>'span5'), 'label' => $label . 'ville' . $this->_extension, 'required' => false,))
+            ->add('pays_id' . $this->_extension, 'country', array('attr'=>array('class'=>'span5'), 'label' => $label . 'pays' . $this->_extension, 'required' => true,));
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
