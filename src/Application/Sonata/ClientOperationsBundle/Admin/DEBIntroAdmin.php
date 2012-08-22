ea8ba5234c2e9270c60c9be63d6fc0dc509f244a
defan <defan@list.ru>
2012-08-22 15:37:21 +0300

    Feature #18 :: Client Operations - Data description DEB Intro

---
+++ src/Application/Sonata/ClientOperationsBundle/Admin/DEBIntroAdmin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Sonata\ClientOperationsBundle\Admin;
+
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+use Application\Sonata\ClientOperationsBundle\Admin\AbstractTabsAdmin as Admin;
+
+class DEBIntroAdmin extends Admin
+{
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+         parent::configureFormFields($formMapper);
+
+        $formMapper
+            ->add('n_ligne', null, array('label' => $this->getFieldLabel('n_ligne')))
+            ->add('nomenclature', null, array('label' => $this->getFieldLabel('nomenclature')))
+            ->add('pays_id_destination', 'country', array('label' => $this->getFieldLabel('pays_id_destination')))
+            ->add('valeur_fiscale', null, array('label' => $this->getFieldLabel('valeur_fiscale')))
+            ->add('regime', null, array('label' => $this->getFieldLabel('regime')))
+            ->add('valeur_statistique', null, array('label' => $this->getFieldLabel('valeur_statistique')))
+            ->add('masse_mette', null, array('label' => $this->getFieldLabel('masse_mette')))
+            ->add('unites_supplementaires', null, array('label' => $this->getFieldLabel('unites_supplementaires')))
+            ->add('nature_transaction', null, array('label' => $this->getFieldLabel('nature_transaction')))
+            ->add('conditions_livraison', null, array('label' => $this->getFieldLabel('conditions_livraison')))
+            ->add('mode_transport', null, array('label' => $this->getFieldLabel('mode_transport')))
+            ->add('departement', null, array('label' => $this->getFieldLabel('departement')))
+            ->add('pays_id_origine', 'country', array('label' => $this->getFieldLabel('pays_id_origine')))
+            ->add('CEE', null, array('label' => $this->getFieldLabel('CEE')))
+            ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        parent::configureListFields($listMapper);
+
+        $listMapper->addIdentifier('id', null)
+            ->add('n_ligne', null, array('label'=> $this->getFieldLabel('n_ligne')))
+            ->add('nomenclature', null, array('label'=> $this->getFieldLabel('nomenclature')))
+            ->add('pays_id_destination', null, array('label'=> $this->getFieldLabel('pays_id_destination')));
+    }
+}
