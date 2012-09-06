dbe1e47b3ebf81246d10f39cded9f2d5c4005026
defan <defan@list.ru>
2012-09-06 20:23:40 +0300

    Review #110 :: Clients Operations - Operations list When a file is uploaded, show a page with errors (if some validations don't pass) or number of lines to be imported. Button "Valider" (imports the file. This button is displayed only if there is no error), button "Annuler" (Cancel the importation). Brings to the main page after. reload page

--- src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
+++ src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
@@ -21,6 +21,7 @@ jQuery(document).ready(function ($) {
             success:function (i) {
                 if (i.status) {
                     function_alert();
+                    window.location.reload();
                 }
             }
         });
