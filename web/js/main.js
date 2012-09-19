bd3a771dcbe1581e17b7d920db9ae1bd60d7dc36
defan <defan@list.ru>
2012-09-19 14:19:36 +0300

    Review #95 :: Client Operations - Form

---
+++ web/js/main.js
@@ -0,0 +1,4 @@
+
+function html_ajax_load() {
+    return '<div id="ajax_loading" style="background:#fff; border:1px solid #dddddd; position:fixed; left:45%; top: 25%; padding:20px 30px; z-index:99999" class="all_loading"><img height="66" width="66" alt=""  src="/img/ajax-loader.gif" /></div>';
+}
