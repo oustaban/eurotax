31461e5ce12230b26d8c82b9d03655bc81c8c40d
defan <defan@list.ru>
2012-10-02 14:32:26 +0300

    Feature #330 :: Clients Operations - Verrouiller - Intermediary step fix bug count

--- sql/trigger/et_client_alert_insert.sql
+++ sql/trigger/et_client_alert_insert.sql
@@ -1,5 +1,6 @@
+DROP TRIGGER IF EXISTS `et_client_alert_insert`;
 CREATE TRIGGER `et_client_alert_insert`
 AFTER INSERT ON `et_client_alert` FOR EACH ROW
 BEGIN
-CALL et_client_alert_add(NEW.client_id);
+CALL et_client_alert_add_del(NEW.client_id);
 END;
--- sql/trigger/et_client_alter_delete.sql
+++ sql/trigger/et_client_alter_delete.sql
@@ -1,5 +1,6 @@
+DROP TRIGGER IF EXISTS `et_client_alter_delete`;
 CREATE TRIGGER `et_client_alter_delete`
 AFTER DELETE ON `et_client_alert` FOR EACH ROW
 BEGIN
-CALL et_client_alert_del(OLD.client_id);
+CALL et_client_alert_add_del(OLD.client_id);
 END;
