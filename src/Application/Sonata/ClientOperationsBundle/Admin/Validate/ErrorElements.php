3668665103373f8c73a91869ce1c298afa6e4b37
defan <defan.hypernaut@gmail.com>
2012-10-26 17:24:57 +0300

    Review #828 :: Client Operations - Modifications

--- src/Application/Sonata/ClientOperationsBundle/Admin/Validate/ErrorElements.php
+++ src/Application/Sonata/ClientOperationsBundle/Admin/Validate/ErrorElements.php
@@ -205,23 +205,25 @@ class ErrorElements
      */
     public function validateDevise()
     {
-        $value = $this->_object->getDevise()->getAlias();
-        if ($value != 'euro') {
-            /* @var $doctrine \Doctrine\Bundle\DoctrineBundle\Registry */
-            $doctrine = \AppKernel::getStaticContainer()->get('doctrine');
-            $em = $doctrine->getManager();
-            /* @var $devise \Application\Sonata\DevisesBundle\Entity\Devises */
-            $devise = $em->getRepository('ApplicationSonataDevisesBundle:Devises')->findOneByDate($this->_object->getDatePieceFormat());
+        if ($this->_object->getDevise()) {
+            $value = $this->_object->getDevise()->getAlias();
+            if ($value != 'euro') {
+                /* @var $doctrine \Doctrine\Bundle\DoctrineBundle\Registry */
+                $doctrine = \AppKernel::getStaticContainer()->get('doctrine');
+                $em = $doctrine->getManager();
+                /* @var $devise \Application\Sonata\DevisesBundle\Entity\Devises */
+                $devise = $em->getRepository('ApplicationSonataDevisesBundle:Devises')->findOneByDate($this->_object->getDatePieceFormat());
 
-            $error = true;
-            if ($devise) {
-                $method = 'getMoney' . ucfirst($value);
-                if (method_exists($devise, $method)) {
-                    $error = !$devise->$method();
+                $error = true;
+                if ($devise) {
+                    $method = 'getMoney' . ucfirst($value);
+                    if (method_exists($devise, $method)) {
+                        $error = !$devise->$method();
+                    }
                 }
-            }
-            if ($error) {
-                $this->errorElement->with('devise')->addViolation('No Devise for this month')->end();
+                if ($error) {
+                    $this->errorElement->with('devise')->addViolation('No Devise for this month')->end();
+                }
             }
         }
 
