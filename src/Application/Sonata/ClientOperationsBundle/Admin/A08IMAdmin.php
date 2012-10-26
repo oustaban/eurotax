286afca7c4726aec058ec8830ecda0280838d444
defan <defan.hypernaut@gmail.com>
2012-10-26 13:52:49 +0300

    Review #811 :: Client Operations - V01-TVA Form

--- src/Application/Sonata/ClientOperationsBundle/Admin/A08IMAdmin.php
+++ src/Application/Sonata/ClientOperationsBundle/Admin/A08IMAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
+use Application\Sonata\ClientOperationsBundle\Admin\Validate\ErrorElements;
 
 use Application\Sonata\ClientOperationsBundle\Admin\AbstractTabsAdmin as Admin;
 
