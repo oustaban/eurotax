da433d7f65810fffe5279b105a56b19be6e4b18f
defan <defan.hypernaut@gmail.com>
2012-11-14 19:33:52 +0200

    Feature #1071 :: Install and configure Behat

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -69,6 +69,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Behat\BehatBundle\BehatBundle();
         }
 
         return $bundles;
