3d4a5111ab35a1b2b97d266528e2091692ff18c2
Volodymyr <volodymyr@nakvasiuk.com>
2012-11-14 14:14:07 +0200

    Bug #1080 :: Undefined property: mPDF::$hasOC

--- src/Application/Tools/mPDF.php
+++ src/Application/Tools/mPDF.php
@@ -6,4 +6,24 @@ include VENDOR_PATH . '/mpdf/mpdf/mpdf.php';
 class mPDF extends \mPDF
 {
     var $hasOC;
+
+    function SetFont($family,$style='',$size=0, $write=true, $forcewrite=false) {
+        $return = parent::SetFont($family,$style,$size, $write, $forcewrite);
+        $this->fixFonts();
+        return $return;
+    }
+
+    function AddCIDFont($family,$style,$name,&$cw,$CMap,$registry,$desc) {
+        $return = parent::AddCIDFont($family,$style,$name,$cw,$CMap,$registry,$desc);
+        $this->fixFonts();
+        return $return;
+    }
+
+    function fixFonts(){
+        foreach ($this->fonts as &$font){
+            if (!isset($font['used'])){
+                $font['used'] = false;
+            }
+        }
+    }
 }
