c56436f9b12c34734e4e231bf3a4c192029b04b6
defan <defan.hypernaut@gmail.com>
2012-11-15 13:29:13 +0200

    mink settings

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -70,6 +70,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Behat\BehatBundle\BehatBundle();
+            $bundles[] = new Behat\MinkBundle\MinkBundle();
         }
 
         return $bundles;
