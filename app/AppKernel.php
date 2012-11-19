c2921d8a177a73929318e9898d6fe80831f079a0
defan <defan@list.ru>
2012-11-19 21:33:19 +0200

    Behat run all test bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -69,6 +69,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Application\Sonata\BehatBundle\ApplicationSonataBehatBundle();
         }
 
         return $bundles;
