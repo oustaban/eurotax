f9b5d3838e83c0ced36f53e557f0bb24d1494cba
defan <defan.hypernaut@gmail.com>
2012-11-21 20:04:22 +0200

    behat commands

--- src/Application/Sonata/BehatBundle/Command/BehatAllCommand.php
+++ src/Application/Sonata/BehatBundle/Command/BehatAllCommand.php
@@ -6,26 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
 
 
-class BehatCommand extends ContainerAwareCommand
+class BehatAllCommand extends ContainerAwareCommand
 {
+    protected $bundles = array(
+        'ClientBundle',
+        'ClientOperationsBundle',
+        'DashboardBundle',
+        'DevisesBundle',
+        //'ErrorsBundle',
+        'ImpotsBundle',
+        'UserBundle',
+    );
+
     protected function configure()
     {
         $this
-            ->setName('behat:bundle')
-            ->setDescription('behat bundle @BundleName')
-           ->setDefinition(array(new InputArgument('name', InputArgument::REQUIRED, 'bundle')))
-        ;
+            ->setName('behat:all')
+            ->setDescription('behat bundle all');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $bundle = $input->getArgument('name');
-        if ($bundle) {
+        foreach ($this->bundles as $bundle) {
             $text = '';
-            $command = 'php ' . ROOT_PATH . '/vendor/behat/behat/bin/behat ' . $bundle;
+            $command = 'php ' . ROOT_PATH . '/vendor/behat/behat/bin/behat @ApplicationSonata' . $bundle;
             exec($command, $text);
             $output->writeln($text);
         }
