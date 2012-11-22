b6a364b969507f660fa45f2bd5472c096570d991
Volodymyr <volodymyr@nakvasiuk.com>
2012-11-22 20:41:20 +0200

    Feature #1181 :: Sonata - Country SEPA list

---
+++ sql/et_list_countries_EU.sql
@@ -0,0 +1,4 @@
+UPDATE et_list_countries SET  EU=NULL;
+UPDATE et_list_countries SET  EU=1 WHERE   code IN(''DE'',''AT'',''BE'',''BG'',''CY'',''DK'',''ES'',''EE'',''FI'',''FR'',''GR'',''HU'',''IE'',''IT'',''LV'',''LT'',''LU'',''MT'',''MC'',''NL'',''PL'',''PT'',''CZ'',''RO'',''GB'',''SK'',''SI'',''SE'');
+UPDATE et_list_countries SET  sepa=NULL;
+UPDATE et_list_countries SET  sepa=1 WHERE code IN(''DE'',''AT'',''BE'',''BG'',''CY'',''DK'',''ES'',''EE'',''FI'',''FR'',''GR'',''HU'',''IE'',''IT'',''LV'',''LT'',''LU'',''MT'',''MC'',''NL'',''PL'',''PT'',''CZ'',''RO'',''GB'',''SK'',''SI'',''SE'',''IS'',''LI'',''NO'',''CH'');
