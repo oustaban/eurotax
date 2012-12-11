0d2ff878bcb1893095472648ee7275e3ab589b41
Volodymyr <volodymyr@nakvasiuk.com>
2012-12-11 17:18:14 +0200

    Feature #1480 :: Client Admin - General - Address field

--- src/Application/Sonata/ClientBundle/Controller/ContactController.php
+++ src/Application/Sonata/ClientBundle/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace Application\Sonata\ClientBundle\Controller;
 
 use Application\Sonata\ClientBundle\Controller\AbstractTabsController as Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 /**
  * Contact controller.
  *
@@ -13,4 +14,13 @@ class ContactController extends Controller
      * @var string
      */
     protected  $_tabAlias = 'contact';
+
+    public function configure()
+    {
+        parent::configure();
+
+        if (!$this->client->getAutreDestinataireDeFacturation()){
+            throw new NotFoundHttpException();
+        }
+    }
 }
