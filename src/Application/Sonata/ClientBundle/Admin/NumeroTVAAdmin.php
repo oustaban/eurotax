e0fd587c5db938bab119b712c1b8c38c17b77cd5
Mitchelle Pascual <mitch.pascual@gmail.com>
2013-07-25 17:54:24 +0800

    Error message in french. Virement PDF layout adjustment.

--- src/Application/Sonata/ClientBundle/Admin/NumeroTVAAdmin.php
+++ src/Application/Sonata/ClientBundle/Admin/NumeroTVAAdmin.php
@@ -68,7 +68,7 @@ class NumeroTVAAdmin extends Admin
     	if (is_null($object->getId()) && $object->getDateDeVerification()) {
     		$date = $object->getDateDeVerification();
     		if( $date->format('Ymd') > date('Ymd')) {
-    			$errorElement->with('date_de_verification')->addViolation('Date should be not greater than today\'s date.')->end();
+    			$errorElement->with('date_de_verification')->addViolation('La date ne peut pas être supérieur à la date du jour.')->end();
     		}
     	}
     }
