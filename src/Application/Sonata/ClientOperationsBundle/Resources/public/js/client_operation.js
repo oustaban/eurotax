08e07ed22acef929c2af6e3c8bb06aabb66b1852
Mitchelle Pascual <mitch.pascual@gmail.com>
2013-12-17 20:51:14 +0800

    http://outils.prodinternet.com/mantis/view.php?id=4351#c9724

--- src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
+++ src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
@@ -845,6 +845,10 @@ function init_rapprochement_sums() {
 	    //if( (totals_input_v1 === 0 && totals_input_v2 === 0) && (totals_output_v1 === 0 && totals_output_v2 === 0)) {
 	    	//$('#rapprochement_form').hide();
 	    //}
+	    
+	    if($('.rapprochement_intro').is(':hidden') && $('.rapprochement_exped').is(':hidden')) {
+	    	$('#btn_locking').hide();
+	    }
     
     }
     
