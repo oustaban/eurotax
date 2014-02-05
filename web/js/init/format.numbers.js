5713524222bfe91195a02115d3038041c6318f3b
Mitchelle Pascual <mitch.pascual@gmail.com>
2014-02-05 23:05:26 +0800

    http://outils.prodinternet.com/mantis/view.php?id=4421

--- web/js/init/format.numbers.js
+++ web/js/init/format.numbers.js
@@ -8,7 +8,8 @@ $(function () {
         if (val == ''){
             return;
         }
-        if ($this.hasClass('sonata-ba-list-field-money') && (!$this.find('div').hasClass('valeur_statistique') && !$this.find('div').hasClass('valeur_fiscale'))) {
+
+        if ($this.hasClass('sonata-ba-list-field-money')) {
         	formattedVal = euro_num_format(real_num(val), 2, true);
         } else  if ($this.hasClass('sonata-ba-list-field-number')) {
         	formattedVal = real_num(val);
@@ -42,19 +43,11 @@ function euro_num_format(rnum, rlength, returnzero) {
 	if(rnum === '') {
 		return '';
 	}
-	var numberStr = rnum.toFixed(rlength).toString().replace('.', ',');
-	var numFormatDec = numberStr.slice(-2); //decimal 00
-	numberStr = numberStr.substring(0, numberStr.length-3); //cut last 3 strings
-	var numFormat = [];
-	while (numberStr.length > 3) {
-		numFormat.unshift(numberStr.slice(-3));
-		numberStr = numberStr.substring(0, numberStr.length-3);
-	}
-	numFormat.unshift(numberStr);
 	
-	var implode = numFormat.join(' ')+','+numFormatDec; //format 000 000 000,00
-	var formatted = implode.replace('- ', '-');
-	return formatted;
+	
+	return number_format(rnum, rlength, ',', ' ');
+	
+	
 }
 
 
@@ -67,3 +60,76 @@ function real_num(num) {
 	
 	return parseFloat(num);
 }
+
+
+
+function number_format (number, decimals, dec_point, thousands_sep) {
+	  // From: http://phpjs.org/functions
+	  // +   original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	  // +     bugfix by: Michael White (http://getsprink.com)
+	  // +     bugfix by: Benjamin Lupton
+	  // +     bugfix by: Allan Jensen (http://www.winternet.no)
+	  // +    revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	  // +     bugfix by: Howard Yeend
+	  // +    revised by: Luke Smith (http://lucassmith.name)
+	  // +     bugfix by: Diogo Resende
+	  // +     bugfix by: Rival
+	  // +      input by: Kheang Hok Chin (http://www.distantia.ca/)
+	  // +   improved by: davook
+	  // +   improved by: Brett Zamir (http://brett-zamir.me)
+	  // +      input by: Jay Klehr
+	  // +   improved by: Brett Zamir (http://brett-zamir.me)
+	  // +      input by: Amir Habibi (http://www.residence-mixte.com/)
+	  // +     bugfix by: Brett Zamir (http://brett-zamir.me)
+	  // +   improved by: Theriault
+	  // +      input by: Amirouche
+	  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	  // *     example 1: number_format(1234.56);
+	  // *     returns 1: '1,235'
+	  // *     example 2: number_format(1234.56, 2, ',', ' ');
+	  // *     returns 2: '1 234,56'
+	  // *     example 3: number_format(1234.5678, 2, '.', '');
+	  // *     returns 3: '1234.57'
+	  // *     example 4: number_format(67, 2, ',', '.');
+	  // *     returns 4: '67,00'
+	  // *     example 5: number_format(1000);
+	  // *     returns 5: '1,000'
+	  // *     example 6: number_format(67.311, 2);
+	  // *     returns 6: '67.31'
+	  // *     example 7: number_format(1000.55, 1);
+	  // *     returns 7: '1,000.6'
+	  // *     example 8: number_format(67000, 5, ',', '.');
+	  // *     returns 8: '67.000,00000'
+	  // *     example 9: number_format(0.9, 0);
+	  // *     returns 9: '1'
+	  // *    example 10: number_format('1.20', 2);
+	  // *    returns 10: '1.20'
+	  // *    example 11: number_format('1.20', 4);
+	  // *    returns 11: '1.2000'
+	  // *    example 12: number_format('1.2000', 3);
+	  // *    returns 12: '1.200'
+	  // *    example 13: number_format('1 000,50', 2, '.', ' ');
+	  // *    returns 13: '100 050.00'
+	  // Strip all characters but numerical ones.
+	  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+	  var n = !isFinite(+number) ? 0 : +number,
+	    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+	    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+	    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+	    s = '',
+	    toFixedFix = function (n, prec) {
+	      var k = Math.pow(10, prec);
+	      return '' + Math.round(n * k) / k;
+	    };
+	  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+	  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+	  if (s[0].length > 3) {
+	    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+	  }
+	  if ((s[1] || '').length < prec) {
+	    s[1] = s[1] || '';
+	    s[1] += new Array(prec - s[1].length + 1).join('0');
+	  }
+	  return s.join(dec);
+	}
