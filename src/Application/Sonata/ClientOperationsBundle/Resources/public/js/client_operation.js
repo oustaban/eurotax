a00ed98f28abb7df10730b60301efabf1cffc1aa
ordinarywebguy <mitch.pascual@gmail.com>
2014-07-30 20:30:27 +0800

    http://outils.prodinternet.com/mantis/view.php?id=4479

--- src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
+++ src/Application/Sonata/ClientOperationsBundle/Resources/public/js/client_operation.js
@@ -642,7 +642,10 @@ function init_rapprochement_sums() {
     			o2 = real_num($('#totals_output_calcu2 b').html());
     		
     		if(i1 != 0 || i2 != 0 || o1 != 0 || o2 != 0) {
-    			$('#btn_locking, #link_declaration').attr('disabled', true);
+    			$('#btn_locking, #link_declaration').attr('disabled', true).on('click', function(e) {
+    				e.preventDefault();
+    				e.stopPropagation();
+    			});
     		} else {
     			$('#btn_locking, #link_declaration').attr('disabled', false);
     		}
